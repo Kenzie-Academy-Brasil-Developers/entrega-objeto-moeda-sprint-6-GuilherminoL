a08876fa97f3ff3bf869f4856ec978884d63543d
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-08-18 16:04:36 -0300

    Adding the 2 last functions and CSS

--- script.js
+++ script.js
@@ -20,3 +20,34 @@ const coin = {
       return image;
     },
   };
+
+  function display20Flips() {
+    document.body.innerHTML = ''
+    const results = [];
+    for (let i = 0; i < 20; i++){
+        coin.flip()
+        const flipResult = coin.toString()
+        results.push(flipResult)
+    }
+    const resultsElement = document.createElement("h3")
+    resultsElement.innerText = results
+    document.body.appendChild(resultsElement)
+    return results
+  }
+
+  function display20Images() {
+    document.body.innerHTML = ''
+    const results = [];
+    for (let i = 0; i < 20; i++){
+        const flipResultImage = document.createElement("img")
+        coin.flip()
+        const flipResult = coin.toString()
+        flipResultImage.src = flipResult +'.png' 
+        results.push(flipResult)
+        document.body.appendChild(flipResultImage)
+    }
+    const resultsElement = document.createElement("h3")
+    resultsElement.innerText = results
+    document.body.appendChild(resultsElement)
+    return results
+  }
